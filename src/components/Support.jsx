b0c149b1433daa70a728ac85893de728bb5ea741
Pearl Rathour <pearl.rathour33@gmail.com>
2023-03-11 14:16:10 +0530

    first commit

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { PhoneIcon, ArrowSmRightIcon } from '@heroicons/react/outline';
 import {ChipIcon, SupportIcon} from '@heroicons/react/solid'
 
-import supportImg from '../assets/support.jpg'
+import supportImg from '../assets/Oakroot.jpg'
 
 const Support = () => {
   return (
